6b87e3dedcad4da36d2909fb208f5700e8e45cee
David Regla <dreglad@gmail.com>
2018-06-03 14:47:29 -0500

    :wrench: Made possible to get float input angle

--- src/game/state.ts
+++ src/game/state.ts
@@ -84,7 +84,7 @@ export default class State {
             player.error = { code: 2, message: 'Número de pieza inválido' };
         } else if (isNaN(force) || force <= 0 || force > config.maxForce) {
             player.error = { code: 3, message: 'Magnitud de la fuerza inválida' };
-        } else if (!Number.isInteger(angle) || angle < 0 || angle > 365) {
+        } else if (isNaN(angle) || angle < 0 || angle > 365) {
             player.error = { code: 4, message: 'Ángulo inválido' };
         }
 
